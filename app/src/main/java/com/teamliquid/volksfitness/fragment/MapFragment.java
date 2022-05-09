e5cd32be91d40ed4dc0339ef2dc2df8adbaef6e4
LiquidSayuki <liquidsayuki@icloud.com>
2022-05-09 16:46:04 +1000

    UGLY!UGLY!UGLY! bug work map feature

--- app/src/main/java/com/teamliquid/volksfitness/fragment/MapFragment.java
+++ app/src/main/java/com/teamliquid/volksfitness/fragment/MapFragment.java
@@ -1,14 +1,31 @@
 package com.teamliquid.volksfitness.fragment;
 
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
 import com.mapbox.geojson.Point;
 import com.mapbox.maps.CameraOptions;
 import com.mapbox.maps.MapView;
@@ -18,27 +35,110 @@ import com.teamliquid.volksfitness.databinding.FragmentMapBinding;
 public class MapFragment extends Fragment {
     private FragmentMapBinding binding;
     private MapView mapView;
+    private FusedLocationProviderClient fusedLocationClient;
 
     public MapFragment(){}
 
+    @SuppressLint("MissingPermission")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding  = FragmentMapBinding.inflate(inflater,container,false);
         View view = binding.getRoot();
-
-        final Point point = Point.fromLngLat(145.045837, -37.876823 );
-
         mapView = binding.mapView;
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
-        CameraOptions cameraPosition = new CameraOptions.Builder()
-                .zoom(13.0)
-                .center(point)
-                .build();
 
-        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
-        mapView.getMapboxMap().setCamera(cameraPosition);
+        // Check permission
+        // If permission granted
+        if(!(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)){
+            // Request for permission
+            ActivityResultLauncher<String[]> locationPermissionRequest =
+                    registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),result -> {
+                        Boolean fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+                        Boolean coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                        if(fineLocationGranted != null && fineLocationGranted){
+                            // Precise location access granted.
+                        }else if (coarseLocationGranted != null && coarseLocationGranted){
+                            // Only approximate location access granted.
+                        }else{
+                            // No location access granted.
+                        }
+                    });
+            locationPermissionRequest.launch(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            onMapReady();
+            refreshMapLocation();
+
+        }
 
         return view;
+    }
+
+    private void onMapReady(){
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
+        mapView.getMapboxMap().setCamera(new CameraOptions.Builder()
+                .zoom(1.0)
+                .build());
+    }
+
+    @SuppressLint("MissingPermission")
+    private void refreshMapLocation(){
+        fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, new CancellationToken() {
+            @NonNull
+            @Override
+            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
+                return null;
+            }
+
+            @Override
+            public boolean isCancellationRequested() {
+                return false;
+            }
+        }).addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    Point point = Point.fromLngLat(location.getLongitude(),location.getLatitude());
+                    mapView.getMapboxMap().setCamera(new CameraOptions.Builder()
+                            .zoom(14.0)
+                            .center(point)
+                            .build());
+                }
+            }
+        });
+    }
+
+    @SuppressLint("Lifecycle")
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @SuppressLint("Lifecycle")
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @SuppressLint("Lifecycle")
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @SuppressLint("Lifecycle")
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
     }
 }
