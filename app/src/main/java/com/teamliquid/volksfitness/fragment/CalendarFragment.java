e0a829b809a4b27aa524fe62edbb277ffa1dfcd3
Xin Zhao <leozhao159@gmail.com>
2022-05-10 16:13:35 +1000

    CalendarFragment and training

--- app/src/main/java/com/teamliquid/volksfitness/fragment/CalendarFragment.java
+++ app/src/main/java/com/teamliquid/volksfitness/fragment/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.teamliquid.volksfitness.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +10,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.teamliquid.volksfitness.R;
 import com.teamliquid.volksfitness.databinding.FragmentCalendarBinding;
+
+import java.util.Calendar;
 
 public class CalendarFragment extends Fragment {
     private FragmentCalendarBinding binding;
 
-    public CalendarFragment (){}
+    public CalendarFragment (){
+
+    }
 
     @Nullable
     @Override
@@ -22,6 +28,17 @@ public class CalendarFragment extends Fragment {
         binding  = FragmentCalendarBinding.inflate(inflater,container,false);
         View view = binding.getRoot();
 
+        binding.calenderButton.setOnClickListener( v ->{
+            Calendar cal = Calendar.getInstance();
+            Intent intent = new Intent(Intent.ACTION_EDIT);
+            intent.setType("vnd.android.cursor.item/event");
+            intent.putExtra("beginTime", cal.getTimeInMillis());
+            intent.putExtra("allDay", true);
+            intent.putExtra("rrule","FREQ=DAILY");
+            intent.putExtra("endTime",cal.getTimeInMillis() + 60*60*1000);
+            intent.putExtra("title","test-event");
+            startActivity(intent);
+        });
 
         return view;
     }
